4fdeb72ba660a79adbfb12a01c4d1858c8c0718f
Edson Mackney <edsonmackney@icloud.com>
2024-02-28 10:38:13 +0300

    insertion to database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => '10.200.222.76',
+        'hostname'     => 'localhost',
         'username'     => 'postgres',
-        'password'     => 'postgres',
-        'database'     => 'onebox',
+        'password'     => 'root',
+        'database'     => 'oneBox',
         'DBDriver'     => 'postgre',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => 5432,
         'numberNative' => false,
     ];
 
